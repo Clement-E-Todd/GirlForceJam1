8fdfa71aa2d6389b2a9358eac5b6ad6b8e20a207
Lucia <lucia.okeh@gmail.com>
2016-09-10 19:12:57 -0400

    Added code for high score

---
+++ Assets/scripts/ScoreManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    Text scoreText;
+    int highScore;
+
+    // Use this for initialization
+    void Start () {
+        scoreText = GetComponent<Text>();
+        PlayerPrefs.SetInt("High Score", 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        highScore = PlayerPrefs.GetInt("High Score");
+        scoreText.text = highScore.ToString();
+    }
+}
