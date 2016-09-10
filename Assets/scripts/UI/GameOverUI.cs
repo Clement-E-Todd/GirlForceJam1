44c8131ef5785393afcda0786124396d17c2e3e3
Jason <jason.nuyens@gmail.com>
2016-09-10 16:46:24 -0400

    Working on lose condition.

---
+++ Assets/scripts/UI/GameOverUI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameOverUI : MonoBehaviour 
+{
+	public List<GameObject> objectsToToggle;
+	private bool isGameOver;
+
+	void Start () 
+	{
+	
+	}
+
+	public void GameOver()
+	{
+		foreach (var obj in objectsToToggle)
+		{
+			obj.SetActive(true);
+		}
+
+		Time.timeScale = 0.0f;
+	}
+
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Time.timeScale = 1.0f;
+
+			foreach (var obj in objectsToToggle)
+			{
+				obj.SetActive(false);
+			}
+		}
+	}
+}
