b1ded48ab5bd13ab32c6eb84e5423dcc2a1b39d1
Clement-E-Todd <clem.e.todd@gmail.com>
2016-09-10 14:39:46 -0400

    [Annie] Ski Movement

---
+++ Assets/scripts/player/SkiMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkiMovement : MonoBehaviour {
+
+	public string TriggerName;
+
+	public Vector3 OuterPosition;
+	public Vector3 InnerPosisition;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		float triggerValue = Input.GetAxis(TriggerName);
+		transform.position = Vector3.Lerp(OuterPosition, InnerPosisition, triggerValue);
+	}
+}
