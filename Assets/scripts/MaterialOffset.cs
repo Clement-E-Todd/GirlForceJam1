f3e3db41cf63018f8a11d761f81978ae4717b9b1
Lucia <lucia.okeh@gmail.com>
2016-09-11 15:59:48 -0400

    Made script for skiway scrolling

---
+++ Assets/scripts/MaterialOffset.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaterialOffset : MonoBehaviour {
+
+    public float scrollSpeed;
+    private Renderer renderer;
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float materialOffset = Time.time * scrollSpeed;
+        Vector2 vect = new Vector2(0, materialOffset);
+
+        renderer.material.SetTextureOffset("_MainTex", vect);
+	}
+}
