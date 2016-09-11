1a663f950e1224a448d9efde61ccd670e4d758c7
Jason <jason.nuyens@gmail.com>
2016-09-11 18:55:25 -0400

    Small ui updates.

---
+++ Assets/scripts/UI/MusicButtonToggle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MusicButtonToggle : MonoBehaviour 
+{
+	public AudioSource MusicSource;
+	public Button uiButton;
+
+	private bool activated;
+
+	public void Toggle()
+	{
+		activated = !activated;
+
+		MusicSource.enabled = activated;
+
+		uiButton.Select();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
