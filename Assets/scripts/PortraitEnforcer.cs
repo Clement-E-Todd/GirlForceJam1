b79c5ac4fd89f7b674b31ab82e667c76571a7532
Unknown <clem.e.todd@gmail.com>
2018-01-20 10:45:29 -0500

    Upgrade project to Unity 5.5

---
+++ Assets/scripts/PortraitEnforcer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortraitEnforcer : MonoBehaviour
+{
+    void Awake()
+    {
+        //DontDestroyOnLoad(gameObject);
+        Screen.SetResolution((int)(Screen.currentResolution.height * 0.5625f), Screen.currentResolution.height, true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Camera.main.aspect = 0.5625f;
+        //Camera.main.ResetProjectionMatrix();
+    }
+}
